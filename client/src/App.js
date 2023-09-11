1601cab4f191ee972ed6439389fe1504e8d5193f
Cristi Miloiu <miloiuc4@gmail.com>
2023-09-11 18:34:29 +0300

    Add loader

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,29 @@ import {
   Navigate,
 } from "react-router-dom";
 import Home from "./views/Home";
+import Preloader from "./components/Preloader";
+import { PreloaderProvider } from "./components/PreloaderProvider";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/membrii" element={<Home />} />
-        <Route path="/evenimente" element={<Home />} />
-        <Route path="/facultate" element={<Home />} />
-        <Route path="/faq" element={<Home />} />
-        <Route path="/contact" element={<Home />} />
-        <Route path="/despre" element={<Home />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
-      </Routes>
-    </Router>
+    <PreloaderProvider>
+      <Preloader />
+      <Router basename="/">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/membrii" element={<Home />} />
+          <Route path="/evenimente" element={<Home />} />
+          <Route path="/facultate" element={<Home />} />
+          <Route path="/faq" element={<Home />} />
+          <Route path="/contact" element={<Home />} />
+          <Route path="/despre" element={<Home />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+      </Router>
+    </PreloaderProvider>
   );
 }
