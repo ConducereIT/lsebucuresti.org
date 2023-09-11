7a04dbc86771ff401734ef645c960fd7feebf52c
MDGcode <adrianboscanici@yahoo.com>
2023-09-11 16:11:02 +0300

    adding stats and footer

---
+++ client/src/components/AnimatedCounter.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useRef } from "react";
+import {
+  animate,
+  motion,
+  useInView,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
+
+export default function AnimatedCounter({ from, to }) {
+  const count = useMotionValue(from);
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+  const ref = useRef(null);
+  const inView = useInView(ref);
+  // while in view, animate the count
+  useEffect(() => {
+    if (inView) {
+      animate(count, to, { duration: 5 });
+    }
+  }, [count, inView, to]);
+  return <motion.span ref={ref}>{rounded}</motion.span>;
+}
