2160a7bcd21a0b7c04a885ae2bc84af3dce44756
Cristi Miloiu <miloiuc4@gmail.com>
2023-09-14 16:15:11 +0300

    add mobile banner

--- client/src/views/Home.view.js
+++ client/src/views/Home.view.js
@@ -2,7 +2,8 @@ import React from "react";
 
 //Components
 import Header from "../components/Header.component";
-import Banner from "../components/Banner.component";
+import Banner from "../components/BannerPC.component";
+import BannerMobile from "../components/BannerMobile.component";
 import Stats from "../components/Stats.component";
 import Footer from "../components/Footer.component";
 import MapCards from "../components/MapCards.component";
@@ -18,7 +19,12 @@ export default function Home() {
   return (
     <>
       <Header />
-      <Banner />
+        <div className="banner-image">
+            <Banner />
+        </div>
+        <div className="banner-video">
+            <BannerMobile />
+        </div>
       <MapCards
         styledObject={styledObjectExample}
         mappingObject={mappingObjectExample}
