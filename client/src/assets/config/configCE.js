b824399a69037dc367b4fffeb7d7bd837316b3b9
MDGcode <adrianboscanici@yahoo.com>
2023-09-15 20:46:08 +0300

    first commit

---
+++ client/src/assets/config/configCE.js
@@ -0,0 +1,32 @@
+import CalinIorga from "../img/Portraits/consiliu_executiv/portrait_calin_iorga.jpg";
+import Teodora from "../img/Portraits/consiliu_executiv/portrait_teodora_mirzan.jpg";
+import Dragu from "../img/Portraits/consiliu_executiv/portrait_andrei_dragu.jpg";
+import Iulia from "../img/Portraits/consiliu_executiv/portrait_iulia_orvas.jpg";
+import Dinut from "../img/Portraits/consiliu_executiv/portrait_cosmin_dinut.jpg";
+export const membersCE = [
+  {
+    image: CalinIorga,
+    nume: "Călin Iorga",
+    title: "Președinte",
+  },
+  {
+    image: Teodora,
+    nume: "Teodora Mîrzan",
+    title: "Vicepreședinte",
+  },
+  {
+    image: Dragu,
+    nume: "Andrei Dragu",
+    title: "Vicepreședinte",
+  },
+  {
+    image: Iulia,
+    nume: "Iulia Orvas",
+    title: "Secretar General",
+  },
+  {
+    image: Dinut,
+    nume: "Cosmin Dinuț",
+    title: "Secretar General",
+  },
+];
--- client/src/views/Echipa.view.js
+++ client/src/views/Echipa.view.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Header from "../components/Header.component";
+import Footer from "../components/Footer.component";
+import Membrii from "../components/Membrii.component";
+import { membersCE } from "../assets/config/configCE";
+import { membersCoord } from "../assets/config/configCoord";
+import { membersAn } from "../assets/config/configAn";
 
 const Echipa = () => {
-    return (
-        <h1>Echipa</h1>
-    );
-}
+  return (
+    <>
+      <Header />
+      <Membrii title="Consiliul Executiv" members={membersCE} grid={5} />
+      <Membrii title="Coordonatori" members={membersCoord} grid={4} />
+      <Membrii title="Consilieri de an" members={membersAn} grid={4} />
+      <Footer />
+    </>
+  );
+};
 
 export default Echipa;
