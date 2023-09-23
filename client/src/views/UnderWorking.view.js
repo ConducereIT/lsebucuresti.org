d7c554e5482551b8e9fe66cdb8382bfd3fbc5ded
AMIhneaa <asmihnea5@gmail.com>
2023-09-23 20:24:15 +0300

    On click event

--- client/src/views/UnderWorking.view.js
+++ client/src/views/UnderWorking.view.js
@@ -2,35 +2,35 @@ import React, { useState } from "react";
 import Banner from "../components/Banner.component";
 import Header from "../components/Header.component";
 
-document.body.addEventListener("onload", focus());
-document.addEventListener("keydown", detectSpaceKey);
-
-function detectSpaceKey(event) {
-  if (event.keyCode == 13) {
-    showTime();
-  }
-}
-
-function showTime() {
-  var curtain = document.getElementById("curtain");
-  curtain.className = "open";
-
-  var scene = document.getElementById("scene");
-  scene.className = "expand";
-
-  var starter = document.getElementById("starter");
-  starter.className = "fade-out";
-
-  setTimeout(function () {
-    starter.style.display = "none";
-  }, 2000);
-}
 const UnderWorking = () => {
+  const [isCurtainOpen, setCurtainOpen] = useState(false);
+
+  function showTime() {
+    var curtain = document.getElementById("curtain");
+    curtain.className = "open";
+
+    var scene = document.getElementById("scene");
+    scene.className = "expand";
+
+    var starter = document.getElementById("starter");
+    starter.className = "fade-out";
+
+    setTimeout(function () {
+      starter.style.display = "none";
+    }, 2000);
+  }
+
   return (
     <>
-      <div id="starter">press enter</div>
+      <div
+        id="starter"
+        onClick={showTime} // Adaugă handler de clic
+        style={{ cursor: "pointer" }}
+      >
+        Apasa aici!
+      </div>
       <div id="scene">
-        <div id="curtain">
+        <div id="curtain" className={isCurtainOpen ? "open" : ""}>
           <div>
             <Header />
             <Banner />
